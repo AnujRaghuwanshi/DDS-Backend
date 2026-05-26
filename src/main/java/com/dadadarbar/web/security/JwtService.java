7ac3bee67cf7e3ef9bd0e0132ed2ca65136a33ed
Anuj <anujraghuwanshipip04@gmail.com>
2026-05-26 22:20:14 +0530

    key from env

--- src/main/java/com/dadadarbar/web/security/JwtService.java
+++ src/main/java/com/dadadarbar/web/security/JwtService.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
@@ -12,10 +13,17 @@ import java.util.Date;
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "mySuperSecretKeyForJwtTokenGeneration123456789";
+
+    private final String SECRET_KEY;
+
     private static final long EXPIRATION = 1000*60*60*24;
 
-    private final Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    private final Key key;
+
+    public JwtService(@Value("${jwt.secret}") String secretKey) {
+        this.SECRET_KEY = secretKey;
+        this.key = Keys.hmacShaKeyFor(this.SECRET_KEY.getBytes());
+    }
 
     public String generateToken(String email){
         return Jwts.builder()

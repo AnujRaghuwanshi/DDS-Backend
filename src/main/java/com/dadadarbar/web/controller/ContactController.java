1c038106c145bc154b8ab4c0063753c028c5a26f
Anuj <anujraghuwanshipip04@gmail.com>
2026-06-22 19:20:41 +0530

    added contact endpoint

---
+++ src/main/java/com/dadadarbar/web/controller/ContactController.java
@@ -0,0 +1,30 @@
+package com.dadadarbar.web.controller;
+
+import com.dadadarbar.web.dto.ApiResponse;
+import com.dadadarbar.web.dto.ContactRequest;
+import com.dadadarbar.web.service.EmailService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/contact")
+@RequiredArgsConstructor
+public class ContactController {
+
+    private final EmailService emailService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<Void>> sendMessage(@RequestBody @Valid ContactRequest req){
+        emailService.sendContactMessage(req);
+        return ResponseEntity.ok(ApiResponse.<Void>builder()
+                .success(true)
+                .message("Message sent successfully")
+                .build()
+        );
+    }
+}
